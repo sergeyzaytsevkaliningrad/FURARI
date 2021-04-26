3cd427ec4f7cef66f22fe0cd1e51359fadc06112
Rudolf Oganesyan <rude97@yandex.ru>
2021-04-26 16:39:47 +0300

    robots list

--- BookHack/Presentation Layer/UsersRaiting/UsersRaiting.swift
+++ BookHack/Presentation Layer/UsersRaiting/UsersRaiting.swift
@@ -116,31 +116,36 @@ struct ARRealityKit: View {
     var body: some View {
         
         VStack {
-            Label("Инструкции", systemImage: "list")
+            Label("Информирование", systemImage: "list")
                 .font(.largeTitle)
                 .accentColor(.red)
                 .colorScheme(.dark)
-//            VStack(alignment: .leading) {
-//                Button("Инфо") {
-//                    isPresented.toggle()
-//                }
-//                .popover(isPresented: $isPresented) {
-//                    nil
-//                }
-//            }
-            .padding()
-            List(instructions) { tutor in
-                //Image(tutor.imageName).cornerRadius(30.0)
-                VStack(alignment: .leading) {
-                    Text(tutor.name)
-                        .font(.title3)
-                    Text(tutor.headline)
-                        .font(.subheadline)
-                        .foregroundColor(.gray)
-//                    Text(tutor.points)
-//                        .foregroundColor(.red)
-                    
-                }
+                //            VStack(alignment: .leading) {
+                //                Button("Инфо") {
+                //                    isPresented.toggle()
+                //                }
+                //                .popover(isPresented: $isPresented) {
+                //                    nil
+                //                }
+                //            }
+                .padding()
+            //            List(instructions) { tutor in
+            //                //Image(tutor.imageName).cornerRadius(30.0)
+            //                VStack(alignment: .leading) {
+            //                    Text(tutor.name)
+            //                        .font(.title3)
+            //                    Text(tutor.headline)
+            //                        .font(.subheadline)
+            //                        .foregroundColor(.gray)
+            ////                    Text(tutor.points)
+            ////                        .foregroundColor(.red)
+            //
+            //                }
+            //            }
+            List(robotsList, children: \.subinfo) { row in
+                Text(row.info)
+                    .bold()
+                    .padding()
             }
             .cornerRadius(25)
             .padding()
@@ -154,3 +159,16 @@ struct ARRealityKit_Previews: PreviewProvider {
         ARRealityKit()
     }
 }
+
+struct RobotsInfo: Identifiable {
+    let id = UUID()
+    let info: String
+    var subinfo: [RobotsInfo]?
+}
+
+let robotsList = [
+    RobotsInfo(info: "UR3", subinfo: [RobotsInfo(info: "The UR3e collaborative robot is a smaller collaborative table-top robot, perfect for light assembly tasks and automated workbench scenarios. The compact table-top cobot weighs only 24.3 lbs (11 kg), but has a payload of 6.6 lbs (3 kg),  ±360-degree rotation on all wrist joints, and infinite rotation on the end joint.")]),
+    RobotsInfo(info: "UR5", subinfo: [RobotsInfo(info: "Built with the future in mind, the UR5e is designed to grow in capability alongside your business, a spring board to improved product quality and productivity, so you will always be able to stay ahead of competition. Equipped with intuitive programming, versatile use and an almost endless list of opportunities for add-ons, the UR5e is able to complement production regardless of your industry, company size or product nature.")]),
+    RobotsInfo(info: "UR10", subinfo: [RobotsInfo(info: "The Universal Robots UR10e is an extremely versatile collaborative industrial robot arm with its high payload (10kg) and long reach capability.  Its 1300mm reach spans wide workspaces without compromising precision or payload performance.  UR10e addresses a wide range of applications in machine tending, palletizing, and packaging.")]),
+    RobotsInfo(info: "UR16", subinfo: [RobotsInfo(info: "The Universal Robots UR16e delivers an impressive 16kg (35.3 lbs.) of payload within a small footprint, and is ideal for use in heavy machine tending, material handling, packaging, and screw and nut driving applications. This powerhouse robot allows for heavier end of arm tooling and multi-part handling, and is especially useful for achieving shorter cycle times.")]),
+]
